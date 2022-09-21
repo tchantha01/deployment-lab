12346985c2a7ca49c8a6edb180f173217c3e2a23
Toky Chanthavong <tchantha01@gmail.com>
2022-09-21 14:43:20 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 5050
+const app = express()
+
+
+app.use(express.json())
+app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+app.use(express.static(path.join(__dirname, '../client')))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+  })
+  
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Docked at port ' + port)})
