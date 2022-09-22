e5a5609533f52558e28b0d88ee0195b45b58ae73
Toky Chanthavong <tchantha01@gmail.com>
2022-09-22 14:25:37 -0600

    link rollbar to repo

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
+const app = express()
 require('dotenv').config()
 
-const port = process.env.PORT || 5050
-const app = express()
+const port = process.env.PORT || 4050
+
 
 
 app.use(express.json())
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '36f7c0e95faf4811942e1f2f0cb13ae2',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+
+
+
 app.use('/', express.static(path.join(__dirname, '../client/index.html')))
 app.use(express.static(path.join(__dirname, '../client')))
 
